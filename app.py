31a2e3d9612175d3a4b97bd6251dbf915134f037
Nour Jandali <nourmshjan@gmail.com>
2022-07-15 01:16:34 +0300

    deploy database

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import psycopg2.extras
 
 app = Flask(__name__)
 
-DB_HOST = "localhost"
-DB_NAME = "bytes-bots"
-DB_USER = "postgres"
-DB_PASS = "root"
+DB_HOST = "ec2-52-20-166-21.compute-1.amazonaws.com"
+DB_NAME = "d2bhgu2oeoc6np"
+DB_USER = "ciqopzuwvoexbs"
+DB_PASS = "1607715c655c26b9e8e966b51cd21fb23640135a839807ec5639afee19628349"
 DB_PORT = "5432"
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
